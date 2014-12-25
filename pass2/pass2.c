58373a884626c0ea5bebf471dea849c090ddd3d6
varun13 <ise5c@monumentmail.com>
2014-12-25 15:44:19 +0000

    third

--- pass2/pass2.c
+++ pass2/pass2.c
@@ -1,118 +1,109 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-#include <stdio.h> 
-#include <string.h> 
-#include <stdlib.h> 
-#define prog_length 10
-int main() 
-{ 
-    FILE *finput,*foptab,*fsymtab,*foutput; 
-      
-    char optab[20][20],opco[20][20],symtab[20][20],syco[20][20]; 
-      char address[20],label[20],opcode[20],operand[20]; 
-      int i=0,j=0,i1=0,j1=0; 
-      int found,found1; 
-  
-    finput=fopen("i2.txt", "r"); 
-      foptab=fopen("o.txt", "r"); 
-      fsymtab=fopen("s2.txt", "r"); 
-      foutput=fopen("p2outp.txt", "w"); 
-  
-    while (!feof(foptab)) 
-      { 
-            fscanf(foptab,"%s\t%s",optab[i],opco[i]); 
-            i++; 
-      } 
-      i-=1; 
-      fclose(foptab); 
-  
-    while (!feof(fsymtab)) 
-      { 
-            fscanf(fsymtab,"%s\t%s",symtab[i1],syco[i1]); 
-            i1++; 
-      } 
-      i1-=1; 
-      fclose(fsymtab); 
-  
-    while(1) 
-      { 
-            found=prog_length; //length of input file
-            found1=prog_length; 
-    
-        fscanf(finput,"%s\t%s\t%s\t%s",address,label,opcode,operand); 
-            
-        if (strcmp(opcode,"START")==0 || strcmp(opcode,"RESB")==0 || strcmp(opcode,"RESW")==0) 
-            { 
-                  fprintf(foutput,"%s\t%s\t%s\t%s\n",address,label,opcode,operand); 
-                  continue; 
-            } 
-            
-        if (strcmp(opcode,"END")==0) 
-            { 
-                  fprintf(foutput,"%s\t%s\t%s\t%s\n",address,label,opcode,operand); 
-                  exit(0); 
-            } 
-            
-        for (j=0;j<i;j++)
-           {
-              if (strcmp(opcode,optab[j])==0)
-              {
-                    found=j;
-              }
-            } 
-            for (j1=0;j1<i1;j1++)
-           {
-              if (strcmp(operand,symtab[j1])==0)
-              {
-                    found1=j1;
-              }
-           } 
-            if (found!=prog_length && found1!=prog_length) 
-            { 
-                  fprintf(foutput,"%s\t%s\t%s\t%s\t%s%s\n",address,label,opcode,operand,opco[found],syco[found1]); 
-            } 
-            
-        else if (strcmp(opcode,"WORD")==0) 
-            { 
-                  int len=strlen(operand); 
-                  int x,k,j; 
-                  char opnew[20];
-            len=6-len; 
-      
-            for (x=0;x<len;x++)
-                 {
-                    opnew[x]='0';
-                 } 
-                  for (k=0;k<strlen(operand);k++)
-                  {
-                    opnew[x++]=operand[k];
-                  } 
-                  opnew[x]='\0'; 
-      
-            fprintf(foutput,"%s\t%s\t%s\t%s\t%X\n",address,label,opcode,operand,atoi(opnew)); 
-        }   
+void main()
+{
+FILE *finput, *foptab, *fsymtab, *foutput;
+char optab[10][10];
+char opco[10][10];
 
-            else if(strcmp(opcode,"BYTE")==0) 
-            { 
-                  int g; 
-                  fprintf(foutput,"%s\t%s\t%s\t%s\t",address,label,opcode,operand); 
-            
-                  for(g=2;g<strlen(operand)-1;g++)
-              {
-                           if(operand[0]=='X')
-                             {
-                           
-                     fprintf(foutput,"%c",operand[g]);
-                  }
-                          else {
-                 fprintf(foutput,"%X",operand[g]);    
-                }    
-              }     
-                  fprintf(foutput,"\n");
-             
-            } 
-      }
- 
-      fclose(foutput); 
-      fclose(finput);
-    
-}  
+char sytab[10][10];
+char syco[10][10];
+char address[10], label[10], opcode[10], operand[10]; 
+int i=0, j;
+int i1=0, j1;
+
+int found, found1;
+
+finput=fopen("i2.txt", "r");
+foptab=fopen("o.txt", "r");
+fsymtab=fopen("s2.txt", "r");
+foutput=fopen("out.txt", "w");
+
+//Populating optab array
+while (!feof(foptab))
+{
+fscanf(foptab, "%s\t%s", optab[i], opco[i]);
+i++;
+}
+i-=1;
+fclose(foptab);
+
+//populating symtab array
+while (!feof(fsymtab))
+{
+fscanf(fsymtab, "%s\t%s", sytab[i1], syco[i1]);
+i1++;
+}
+i1-=1;
+fclose(fsymtab);
+
+//Generating machine codes
+while(1)
+{
+found=11;
+found1=11;
+
+fscanf(finput, "%s\t%s\t%s\t%s", address, label, opcode, operand);
+
+//If opcode is start do nothing, if its end exit the program
+if (strcmp(opcode, "START")==0 || strcmp(opcode, "RESB")==0 || strcmp(opcode, "RESW")==0)
+{
+fprintf(foutput, "%s\t%s\t%s\t%s\n", address, label, opcode, operand);
+continue;
+}
+if (strcmp(opcode, "END")==0)
+{
+fprintf(foutput, "%s\t%s\t%s\t%s\n", address, label, opcode, operand);
+exit(0);
+}
+
+//search opcode
+for (j=0;j<i;j++){if (strcmp(opcode, optab[j])==0){found=j;}}
+
+//search symtab
+for (j1=0;j1<i1;j1++){if (strcmp(operand, sytab[j1])==0){found1=j1;}}
+
+
+if (found!=11 && found1!=11)
+{
+fprintf(foutput, "%s\t%s\t%s\t%s\t%s%s\n", address, label, opcode, operand, opco[found], syco[found1]);
+}
+
+else if(found!=11 && found1==11) // Link addressing
+{
+char newop[10];
+strcpy(newop, operand);
+operand[strlen(operand)-2]='\0';
+
+//search symtab again
+for (j1=0;j1<i1;j1++){if (strcmp(operand, sytab[j1])==0){found1=j1;}}
+int a=(int)strtol(strcat(opco[found],syco[found1]), NULL, 16);
+a+=0x8000;
+fprintf(foutput, "%s\t%s\t%s\t%s\t%x\n", address, label, opcode, newop, a);
+}
+
+else if (strcmp(opcode, "WORD")==0)
+{
+int len=strlen(operand);
+int x, k;
+len=6-len;
+char opnew[10];
+for (x=0;x<len;x++){opnew[x]='0';}
+for (k=0;k<strlen(operand);k++){opnew[x++]=operand[k];}
+opnew[x]='\0';
+fprintf(foutput, "%s\t%s\t%s\t%s\t%s\n", address, label, opcode, operand, opnew);
+}
+else if (strcmp(opcode, "BYTE")==0)
+{
+int g;
+fprintf(foutput, "%s\t%s\t%s\t%s\t", address, label, opcode, operand);
+for (g=2;g<strlen(operand)-1;g++){fprintf(foutput, "%x", operand[g]);}
+fprintf(foutput, "\n");
+}
+}
+
+fclose(foutput);
+fclose(finput);
+}
